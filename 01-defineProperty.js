48c3caf3c477c533314c66e4bd0cc5781905033d
starof <925309723@qq.com>
2020-07-06 20:31:32 +0800

    reactive

--- 01-defineProperty.js
+++ 01-defineProperty.js
@@ -7,6 +7,8 @@
 
 //1. Object.defineProperty()
 function defineReactive(obj,key,val){
+    //val可能是对象，就需要递归处理
+    observe(val)
     Object.defineProperty(obj,key,{
         get(){
             console.log('get',val);
@@ -15,16 +17,64 @@ function defineReactive(obj,key,val){
         set(newVal){ //在这里通知更新页面
             if(newVal!=val){
                 console.log('set',newVal);
-                val = newVal;
+                //newVal可能是一个全新的对象
+                observe(newVal)
+                val = newVal
+                
             }
         }
     })
 }
 
-const obj={}
 
-defineReactive(obj,'foo','foo')
+//对象响应式处理
 
-obj.foo
-// obj.set('fooooooo')//obj.set is not a function
-obj.foo='fooooooooooo'
+function observe(obj){
+    //判断obj类型必须是对象
+    if(typeof obj != 'object' || obj==null){
+        return;
+    }
+    Object.keys(obj).forEach(key=>{
+        defineReactive(obj,key,obj[key])
+    })
+}
+
+
+//让属性转正，让新属性能够被拦截到
+function set(obj, key, val){
+    defineReactive(obj,key,val)
+}
+
+
+
+
+
+
+
+
+
+
+const obj={foo:'foo',bar:'bar',baz:{a:1}}
+
+// defineReactive(obj,'foo','foo')
+
+observe(obj)
+
+// obj.foo
+// // obj.set('fooooooo')//obj.set is not a function
+// obj.foo='fooooooooooo'
+
+// obj.bar
+
+// obj.baz.a=10
+
+//用户直接替换对象，国家队没有受过社会主义教育
+// obj.baz={a:10}
+// obj.baz.a = 100
+
+// obj.dong='dong' //不能这么写，得用提供的动态的set方法
+set(obj,'dong','dong')
+obj.dong
+
+
+//设置数组的时候，改了数组的原型，让它的行为变的可拦截
